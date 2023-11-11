1873b56e1502dcd418804f756b8a2a0ee1936173
Prachijadhav1455 <127278802+Prachijadhav1455@users.noreply.github.com>
2023-11-11 19:10:53 +0530

    Call_By_Value

---
+++ Function_Calling_Techinques/02_Call_By_Value.c
@@ -0,0 +1,26 @@
+
+    using namespace std;
+
+    #include<iostream>
+    #include<conio.h>
+    void Referenc(int&);
+
+    int main()
+    {
+        int No = 10;
+
+        cout<< No << end1;
+
+        Reference(No);
+
+        cout<< No << end1;
+
+        getch();
+        return 0;
+    }
+
+    void value(int i)
+    {
+        cout << "Inside Function ....."<< end1;
+        ++;
+    }
